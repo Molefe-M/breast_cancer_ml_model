647e199cc4ac71d76004389b7b3fe8bf5a7c9914
Molefe-M <molefemohale@gmail.com>
2025-02-17 11:26:21 +0200

    Add unittest

---
+++ tests/integration/test_minimal.py
@@ -0,0 +1,9 @@
+# tests/integration/test_minimal.py
+import unittest
+
+class TestMinimal(unittest.TestCase):
+    def test_something(self):
+        self.assertTrue(True)  # A very simple test
+
+if __name__ == "__main__":
+    unittest.main()
